7f62c7a637f8a924b672a0150bb5c904cbea20dd
leodanuarta <leo.535210024@stu.untar.ac.id>
2023-05-25 23:20:47 +0700

    update API endpoint

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,7 +1,5 @@
 class AppwriteConstants {
   static const String databaseId = '646f7c9bdca68bac75a0';
   static const String projectId = '646cda321effe0caaa7b';
-  static const String endPoint =
-      'https://cloud.appwrite.io/console/project-646cda321effe0caaa7b';
+  static const String endPoint = 'https://cloud.appwrite.io/v1';
 }
-
