1f6f216fa0dab419f3ad5c4c6226a2837247251f
leodanuarta <leo.535210024@stu.untar.ac.id>
2023-05-25 23:13:59 +0700

    beebeer app template

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,7 @@
+class AppwriteConstants {
+  static const String databaseId = '646f7c9bdca68bac75a0';
+  static const String projectId = '646cda321effe0caaa7b';
+  static const String endPoint =
+      'https://cloud.appwrite.io/console/project-646cda321effe0caaa7b';
+}
+
---
+++ lib/constants/constants.dart
@@ -0,0 +1,8 @@
+export './appwrite_constants.dart';
+export './assets_constants.dart';
+
+
+// import "appwrite_constants.dart"
+// import "assets_constants.dart"
+
+// import "constants.dart"
