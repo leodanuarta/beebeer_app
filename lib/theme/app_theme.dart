6ca7116a54ec41d0f948bb5a8d6e2fc1a2271103
reginamarthaa <regina.535210040@stu.untar.ac.id>
2023-05-28 02:34:27 +0700

    login and sign up ui (54:23)

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,14 +2,14 @@ import 'package:beebeer_app/theme/pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData theme = ThemeData.dark().copyWith(
-    scaffoldBackgroundColor: Pallete.backgroundColor,
+  static ThemeData theme = ThemeData.light().copyWith(
+    // scaffoldBackgroundColor: Pallete.backgroundColor,
     appBarTheme: const AppBarTheme(
-      backgroundColor: Pallete.backgroundColor,
+      backgroundColor: Pallete.whiteColor,
       elevation: 0,
     ),
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      backgroundColor: Pallete.blueColor,
+      backgroundColor: Pallete.pinkColor,
     ),
   );
 }
